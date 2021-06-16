c83260120f9680b0c2e7cc73d56dbb9ec7199603
Felipe Souza <felipedb91@gmail.com>
2021-06-15 22:56:32 -0300

    feat: implements faunadb

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,25 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
-
+import { signIn, useSession, signOut } from 'next-auth/client';
 const SignInButton: React.FC = () => {
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.signInButton}>
+  const [session] = useSession();
+  return session ? (
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361  " />
-      Felipe Souza
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417  " />
       Sign in with GitHub
     </button>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import { AppProps } from 'next/app';
+import { Provider as NextAuthProvider } from 'next-auth/client';
 import { Header } from '../components/Header';
 import '../styles/global.scss';
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
+    <NextAuthProvider session={pageProps.session}>
       <Header />
-      <Component {...pageProps} />{' '}
-    </>
+      <Component {...pageProps} />
+    </NextAuthProvider>
   );
 };
 
